6b7b9f99bf20a15109f96d62022eaf99726a692e
James Thomas <james9074@gmail.com>
2014-02-16 17:10:42 -0500

    Email

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,15 +1,15 @@
 class TaskMailer < ActionMailer::Base
-  default from: 'no-reply@railscrm.com'
+  default from: 'no-reply@crm.eastmetrosupply.com'
 
   def notify_new_task(user, task)
     @user = user
     @task = task
-    mail(to: @user.email, subject: 'A new task has been assigned to you on RailsCRM')
+    mail(to: @user.email, subject: 'A new task has been assigned to you at EMS')
   end
 
   def notify_updated_task(user, task)
     @user = user
     @task = task
-    mail(to: @user.email, subject: 'A task you have been assigned to on RailsCRM has been updated')
+    mail(to: @user.email, subject: 'A task you have been assigned to on the EMS CRM has been updated')
   end
 end
