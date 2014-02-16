2814b2ac71b6f2c8adc8c2f68090f95a57762152
James Thomas <james9074@gmail.com>
2014-02-16 17:22:23 -0500

    Email2

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,12 @@ require 'gibberish'
 SassRailscrm::Application.initialize!
 ::KEY = Gibberish::AES.new("awerwrREWdfER1645")
 
-
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
